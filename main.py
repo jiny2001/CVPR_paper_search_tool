29d4ff1e1be6685cce702e0922811f68a76bbe7b
Unknown <jin.yamanaka@gmail.com>
2018-05-28 15:20:01 -0700

    add visualize function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+
 import jinja2
 import webapp2
+
 from model.paper2vec import Paper2Vec
 
 PAGE_TITLE = "ICCV2017"
@@ -9,6 +11,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
 	extensions=['jinja2.ext.autoescape'],
 	autoescape=True)
+
 
 class PaperSearchResults():
 
@@ -24,6 +27,7 @@ class BaseHandler(webapp2.RequestHandler):
 	def render(self, html, values={}):
 		template = JINJA_ENVIRONMENT.get_template(html)
 		self.response.write(template.render(values))
+
 
 class MainPage(BaseHandler):
 
@@ -47,15 +51,15 @@ class MainPage(BaseHandler):
 
 		for result in results:
 			paperInfo = PaperSearchResults(result[0], result[1],
-			                      p2v.paper[result[0]].title,
-			                      p2v.paper[result[0]].abstract_url,
-			                      p2v.paper[result[0]].pdf_url)
+			                               p2v.paper[result[0]].title,
+			                               p2v.paper[result[0]].abstract_url,
+			                               p2v.paper[result[0]].pdf_url)
 			paperSearchResult.append(paperInfo)
 
 		if len(results) > 0:
-			message = ("%d papers found. [ " + keywords_text +" ]") % len(results)
+			message = ("%d papers found. [ " + keywords_text + " ]") % len(results)
 		else:
-			message = "No papers found. [ " + keywords_text +" ]"
+			message = "No papers found. [ " + keywords_text + " ]"
 		values = {
 			'title': PAGE_TITLE,
 			'searchResult': paperSearchResult,
@@ -63,6 +67,7 @@ class MainPage(BaseHandler):
 		}
 
 		self.render("html/main.html", values)
+
 
 class FindSimilarPaperPage(BaseHandler):
 
@@ -73,24 +78,24 @@ class FindSimilarPaperPage(BaseHandler):
 			self.redirect('/')
 
 		target = PaperSearchResults(paper_id, 0,
-			                      p2v.paper[paper_id].title,
-			                      p2v.paper[paper_id].abstract_url,
-			                      p2v.paper[paper_id].pdf_url)
+		                            p2v.paper[paper_id].title,
+		                            p2v.paper[paper_id].abstract_url,
+		                            p2v.paper[paper_id].pdf_url)
 
 		results = p2v.find_similar_papers(paper_id, 5)
 
 		paperSearchResult = []
 		for result in results:
 			paperInfo = PaperSearchResults(result[0], result[1],
-			                      p2v.paper[result[0]].title,
-			                      p2v.paper[result[0]].abstract_url,
-			                      p2v.paper[result[0]].pdf_url)
+			                               p2v.paper[result[0]].title,
+			                               p2v.paper[result[0]].abstract_url,
+			                               p2v.paper[result[0]].pdf_url)
 			paperSearchResult.append(paperInfo)
 
 		values = {
 			'title': PAGE_TITLE,
 			'paper': target,
-			'searchResult':paperSearchResult
+			'searchResult': paperSearchResult
 		}
 		self.render("html/paper.html", values)
 
--- model/paper2vec.py
+++ model/paper2vec.py
@@ -32,11 +32,12 @@ class Paper2Vec:
 
 		print('Saved %d papers info.' % self.papers)
 
-	def load_paper_vectors(self):
+	def load_paper_vectors(self, load_cluster_ids = False):
 
 		self.paper_vectors = helper.load_object(self.model_dir, 'paper_vectors')
 		self.paper = helper.load_object(self.model_dir, 'paper_info')
-		self.paper_cluster_ids = helper.load_object(self.model_dir, 'cluster_ids')
+		if load_cluster_ids:
+			self.paper_cluster_ids = helper.load_object(self.model_dir, 'cluster_ids')
 		self.papers = min(self.paper_vectors.shape[0], len(self.paper))
 
 		print('Loaded %d papers info.' % self.papers)
